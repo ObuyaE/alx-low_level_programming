9d8f5d2a06a407fac9b4cbb6c51415a6baa889a7
ObuyaE <evansotieno.obuya@gmail.com>
2023-02-22 11:28:57 -0800

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add -  adds two integers and returns the result
+ * @a: int to add to b
+ * @b: int to add to a
+ * Return: Always 0 (Success)
+ */
+
+int add(int a, int b)
+
+{
+	return (a + b);
+}
